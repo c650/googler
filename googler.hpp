3299de43bfa77e3b2fef87a9ef7ea6651aeae66e
c650 <charlesbailey@protonmail.com>
2016-12-23 14:00:21 -0500

    modularize

--- googler.hpp
+++ googler.hpp
@@ -6,35 +6,76 @@
 
 #include "./json.hpp"
 
-size_t write_to_string(void *ptr, size_t size, size_t nmemb, std::string& stream);
+#define GOOGLE_CONFIG_PATH "./google_search_api_info.json"
 
-void get_json_from_web(const std::string& url , nlohmann::json& json_obj) {
+namespace Googler {
 
-	CURL *curl = curl_easy_init();
-	if (curl) {
+	void get_json_from_web(const std::string& url , nlohmann::json& json_obj);
+	size_t write_to_string(void *ptr, size_t size, size_t nmemb, std::string& stream);
 
-		curl_easy_setopt(curl, CURLOPT_URL, url.data());
+	/*
+		Formats num_res_to_show results into
 
-		std::string response;
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_string);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, response);
+		title
+			snippet
+			link
+	*/
+	void do_google_search(const std::string& formatted_query, const size_t& num_res_to_show, std::vector<std::string>& res_vec, const std::string& api_key_path = "");
 
-		CURLcode res = curl_easy_perform(curl);
-		if (res == CURLE_OK) {
-			json_obj = nlohmann::json::parse(response);
+// ------------------------------------------------------------------------------
+
+	void get_json_from_web(const std::string& url , nlohmann::json& json_obj) {
+
+		CURL *curl = curl_easy_init();
+		if (curl) {
+
+			curl_easy_setopt(curl, CURLOPT_URL, url.data());
+
+			std::string response;
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_string);
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, response);
+
+			CURLcode res = curl_easy_perform(curl);
+			if (res == CURLE_OK) {
+				json_obj = nlohmann::json::parse(response);
+			}
+			curl_easy_cleanup(curl);
 		}
-		curl_easy_cleanup(curl);
 	}
-}
 
-size_t write_to_string(void *ptr, size_t size, size_t nmemb, std::string& stream) {
-	size_t realsize = size * nmemb;
-	std::string temp(static_cast<const char*>(ptr), realsize);
-	stream.append(temp);
-	return realsize;
-}
+	size_t write_to_string(void *ptr, size_t size, size_t nmemb, std::string& stream) {
+		size_t realsize = size * nmemb;
+		std::string temp(static_cast<const char*>(ptr), realsize);
+		stream.append(temp);
+		return realsize;
+	}
 
-void get_json_from_file(const std::string& file, nlohmann::json& json_obj) {
-	std::fstream fs(file, std::fstream::in);
-	fs >> json_obj;
-}
+	void do_google_search(const std::string& formatted_query, const size_t& num_res_to_show, std::vector<std::string>& res_vec, const std::string& api_key_path) {
+
+		// requires a JSON file with google API stuff
+		nlohmann::json google_api_info;
+
+		try {
+			std::fstream fs( api_key_path.empty() ? GOOGLE_CONFIG_PATH : api_key_path, std::fstream::in);
+			fs >> google_api_info;
+		} catch (...) {
+			std::cerr << "File IO failed. Couldn't retrieve API Key.\n";
+			return;
+		}
+
+		nlohmann::json search_result;
+		get_json_from_web(google_api_info["address"].get<std::string>()
+							+ "key=" + google_api_info["key"].get<std::string>()
+							+ "&cx=" + google_api_info["cx"].get<std::string>()
+							+ "&q=" + formatted_query, search_result);
+
+		size_t count = search_result["items"].size() > 3 ? 3 : search_result["items"].size();
+		for (auto& item : search_result["items"]) {
+
+			res_vec.push_back( item["title"].get<std::string>() + "\n\t" + item["snippet"].get<std::string>() + "\n\t" + item["link"].get<std::string>() );
+
+			if (!(count--))
+				break;
+		}
+	}
+};
--- main.cpp
+++ main.cpp
@@ -7,46 +7,12 @@
 #include "./json.hpp"
 #include "./googler.hpp"
 
-#define GOOGLE_CONFIG_PATH "./google_search_api_info.json"
-/*
-	Formats num_res_to_show results into
-
-	title
-		snippet
-		link
-*/
-void do_google_search(const std::string& formatted_query, const size_t& num_res_to_show, std::vector<std::string>& res_vec);
-
 int main(void) {
 
 	std::vector<std::string> res_vec;
-	do_google_search("test", 3 , res_vec);
+	Googler::do_google_search("test", 3 , res_vec);
 
 	for (auto& result : res_vec)
 		std::cout << result << "\n\n";
-
-}
-
-void do_google_search(const std::string& formatted_query, const size_t& num_res_to_show, std::vector<std::string>& res_vec) {
-
-	// requires a JSON file with google API stuff
-
-	nlohmann::json google_api_info;
-	get_json_from_file( GOOGLE_CONFIG_PATH , google_api_info);
-
-	nlohmann::json search_result;
-	get_json_from_web(google_api_info["address"].get<std::string>()
-						+ "key=" + google_api_info["key"].get<std::string>()
-						+ "&cx=" + google_api_info["cx"].get<std::string>()
-						+ "&q=" + formatted_query, search_result);
-
-	size_t count = search_result["items"].size() > 3 ? 3 : search_result["items"].size();
-	for (auto& item : search_result["items"]) {
-
-		res_vec.push_back( item["title"].get<std::string>() + "\n\t" + item["snippet"].get<std::string>() + "\n\t" + item["link"].get<std::string>() );
-
-		if (!(count--))
-			break;
-	}
 
 }
